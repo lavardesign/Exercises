f9f6ccdadf8b0462a1a7ecb9e84b94c47e9a9cd7
Lavar.Design <bjorn.osterholm@elnotorios.com>
2019-10-08 18:24:17 +0200

    Add project files.

---
+++ Exercises/Classes/Chapter04/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercises.Classes.Chapter04
+{
+    public class Calculator
+    {
+        public double Add(double value1, double value2)
+        {
+            var result = value1 + value2;
+            return result;
+        }
+
+        public double Substract(double value1, double value2)
+        {
+            return value1 - value2;
+        }
+
+        public double Divide(double value1, double value2)
+        {
+            return value1 / value2;
+        }
+
+        public double Multiply(double value1, double value2)
+        {
+            return value1 * value2;
+        }
+    }
+}
