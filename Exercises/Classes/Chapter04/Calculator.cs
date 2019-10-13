fd4002a39b1baddbe32e61412969b492cdd34af2
Lavar.Design <bjorn.osterholm@elnotorios.com>
2019-10-13 19:07:21 +0200

    04-01f

--- Exercises/Classes/Chapter04/Calculator.cs
+++ Exercises/Classes/Chapter04/Calculator.cs
@@ -7,9 +7,20 @@ namespace Exercises.Classes.Chapter04
 {
     public class Calculator
     {
+        private string Output(double value1, double value2, double result, char @operator)
+        {
+            return $"{value1} {@operator} {value2} = {result}";
+        }
+
         public double Add(double value1, double value2)
         {
             var result = value1 + value2;
+            return result;
+        }
+        public double Add(double value1, double value2, out string output)
+        {
+            var result = Add(value1, value2);
+            output = Output(value1, value2, result, '+');
             return result;
         }
 
@@ -17,15 +28,33 @@ namespace Exercises.Classes.Chapter04
         {
             return value1 - value2;
         }
+        public double Substract(double value1, double value2, out string output)
+        {
+            var result = Substract(value1, value2);
+            output = Output(value1, value2, result, '-');
+            return result;
+        }
 
         public double Divide(double value1, double value2)
         {
             return value1 / value2;
         }
+        public double Divide(double value1, double value2, out string output)
+        {
+            var result = Divide(value1, value2);
+            output = Output(value1, value2, result, '/');
+            return result;
+        }
 
         public double Multiply(double value1, double value2)
         {
             return value1 * value2;
+        }
+        public double Multiply(double value1, double value2, out string output)
+        {
+            var result = Multiply(value1, value2);
+            output = Output(value1, value2, result, 'x');
+            return result;
         }
     }
 }
