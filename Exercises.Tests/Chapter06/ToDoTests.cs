a5256a57433ff9a41ff26426ababdae8c3b237ed
Lavar.Design <bjorn.osterholm@elnotorios.com>
2019-10-22 17:55:33 +0200

    Chapter06

--- Exercises.Tests/Chapter06/ToDoTests.cs
+++ Exercises.Tests/Chapter06/ToDoTests.cs
@@ -15,5 +15,55 @@ namespace Exercises.Tests.Chapter06
 
             Assert.NotNull(todo);
         }
+
+        [Fact]
+        public void CanReadDescriptionAndCollectionsAreEmpty()
+        {
+            var todo = new ToDo("Todo 1");
+
+            Assert.Equal("Todo 1", todo.GetDescription());
+            Assert.Empty(todo.GetCheckboxAttributes());
+            Assert.Empty(todo.GetDescriptionAttributes());
+        }
+
+        [Fact]
+        public void CollectionsHaveValues()
+        {
+            var todo = new ToDo("Todo 1");
+            todo.CheckAttributes();
+
+            Assert.NotEmpty(todo.GetCheckboxAttributes());
+            Assert.NotEmpty(todo.GetDescriptionAttributes());
+        }
+
+        [Fact]
+        public void CanCreateTodosInstance()
+        {
+            var todos = new Todos();
+
+            Assert.NotNull(todos);
+        }
+
+        [Fact]
+        public void CanAddAndGetTodos()
+        {
+            var todos = new Todos();
+            todos.Add("todo 1");
+
+            Assert.NotEmpty(todos.Get());
+        }
+
+        [Fact]
+        public void CanRemoveTodos()
+        {
+            var todos = new Todos();
+            todos.Add("todo 1");
+            var countBeforeDelete = todos.Get().Count;
+            var todo = todos.Get().Find(t => t.GetDescription().Equals("todo 1"));
+            todos.Remove(todo);
+
+            Assert.Equal(1, countBeforeDelete);
+            Assert.Empty(todos.Get());
+        }
     }
 }
